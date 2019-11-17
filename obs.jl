81ca6cc2da3bb7d74ce667cfce2f16e39decfc42
Joseph Goldfrank <36973691+goldfrank@users.noreply.github.com>
2019-11-17 11:45:53 -0800

    Add files via upload

--- obs.jl
+++ obs.jl
@@ -7,11 +7,11 @@ function obs1(state)
         rel_brg += 360 
     end
     if ((60 < rel_brg < 90) || (270 < rel_brg < 300)) && (range < SENSOR_RANGE/2)
-        return 1
+        return 1::Int64
     elseif ((60 < rel_brg < 90) || (270 < rel_brg < 300)) && (range < SENSOR_RANGE)
         return 2-2*range/SENSOR_RANGE
     end
-    return 0
+    return 0::Int64
 end
     
 function obs2(state)
@@ -19,11 +19,11 @@ function obs2(state)
     range = state[1]
     if rel_brg < 0 rel_brg += 360 end
     if ((90 <= rel_brg < 120) || (240 < rel_brg <= 270)) && (range < SENSOR_RANGE/2)
-        return 1
+        return 1::Int64
     elseif ((90 <= rel_brg < 120) || (240 < rel_brg <= 270)) && (range < SENSOR_RANGE)
         return 2-2*range/SENSOR_RANGE
     end
-    return 0
+    return 0::Int64
 end
         
 function obs3(state)
@@ -31,11 +31,11 @@ function obs3(state)
     range = state[1]
     if rel_brg < 0 rel_brg += 360 end
     if (120 <= rel_brg <= 240) && (range < SENSOR_RANGE/2)
-        return 1
+        return 1::Int64
     elseif (120 <= rel_brg <= 240) && (range < SENSOR_RANGE)
         return 2-2*range/SENSOR_RANGE
     end
-    return 0    
+    return 0::Int64
 end        
 
 function obs0(state)
@@ -43,10 +43,10 @@ function obs0(state)
     range = state[1]
     if rel_brg < 0 rel_brg += 360 end
     if (rel_brg <= 60) || (rel_brg >= 300) || (range >= SENSOR_RANGE)
-        return 1
+        return 1::Int64
     end
     if (!(obs1(state) > 0) && !(obs2(state) > 0) && !(obs3(state) > 0))
-        return 1 
+        return 1::Int64
     elseif (120 <= rel_brg <= 240) && (SENSOR_RANGE/2 < range < SENSOR_RANGE)
         return 2*range/SENSOR_RANGE - 1
     elseif ((90 <= rel_brg < 120) || (240 < rel_brg <= 270)) && (SENSOR_RANGE/2 < range < SENSOR_RANGE)
@@ -54,7 +54,7 @@ function obs0(state)
     elseif ((60 <= rel_brg < 90) || (270 < rel_brg <= 300)) && (SENSOR_RANGE/2 < range < SENSOR_RANGE)
         return 2*range/SENSOR_RANGE - 1
     end
-    return 0
+    return 0::Int64
 end
 
 function g(x, a, xp, o)
@@ -66,6 +66,6 @@ end
 
 function h(x, rng)
     weights = [obs0(x), obs1(x), obs2(x), obs3(x)]
-    obsers = [0, 1, 2, 3]
-    return sample(obsers, Weights(weights))
+    obsers::Array{Int64,1} = [0, 1, 2, 3]
+    return sample(obsers, Weights(weights))::Int64
 end
